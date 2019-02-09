463f0b8e7fb53d4d52d2cc15811859bcb6096626
Steven Tammen <steven@steventammen.com>
2019-02-09 10:53:50 -0500

    Put things in functions.

--- parse.py
+++ parse.py
@@ -1,4 +1,24 @@
-remap = open("remap.ahk", "w")
-str="test the writing to file"
-remap.write(str)
-remap.close()
+def create_mapping(keymap):
+	remap = open("remap.ahk", "w")
+
+	write_str = ""
+	keys = ["a", "b", "c", "d"]
+	for key in keys:
+		write_str += key + "::" + "\n"
+
+	remap.write(write_str)
+	remap.close()
+
+
+def parse(json_file):
+	layout = {}
+	return layout
+
+
+def main():
+	physical = parse("/layout-json/physical.json")
+	base = parse("/layout-json/base.json")
+	shift = parse("/layout-json/shift.json")
+	num = parse("/layout-json/num.json")
+
+	keymap = {"physical": physical, "base": base, "shift": shift, "num": num}
